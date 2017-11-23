35bbc0a3f64089551979146e1e5cbf5c695ea5bd
Ying <yingpublic@users.noreply.github.com>
2017-11-23 08:55:07 -0500

    Question 1 Finished.

--- HW0_numpy/Q1.py
+++ HW0_numpy/Q1.py
@@ -4,7 +4,21 @@ import sys
 import numpy as np
 
 matrixA = np.loadtxt("matrixA.txt", dtype='i', delimiter=',')
-print(matrixA)
+# print(matrixA)
 
 matrixB = np.loadtxt("matrixB.txt", dtype='i', delimiter=',')
-print(matrixB)
+# print(matrixB)
+
+# ans = np.matmul(matrixA,matrixB)
+ans = np.dot(matrixA,matrixB)
+#print(ans)
+
+#ans2 = sorted(ans)
+#print(ans2)
+
+ans.sort()
+#print(ans)
+print(*ans, sep="\n")
+sys.stdout=open("ans_one.txt","w")
+print(*ans, sep="\n")
+sys.stdout.close()
